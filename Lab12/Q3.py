7bf8089796ccc313bf28c85caaac49e6cb524f68
ToiLaHao1 <haonh@innotech-vn.com>
2026-05-05 10:49:42 +0700

    update remove things

--- Lab12/Q3.py
+++
@@ -1,8 +0,0 @@
-def area_of_triangle(base, height):
-    return 0.5 * base * height
-
-base = float(input("Enter the base of the triangle: "))
-height = float(input("Enter the height of the triangle: "))
-
-area = area_of_triangle(base, height)
-print(f"The area of the triangle is: {area}")
